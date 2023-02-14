929e2708db1fce1e7997bad90cb593779c4e7ce0
durzal1 <67489054+durzal1@users.noreply.github.com>
2023-02-14 17:19:56 -0500

    fefe

--- StrokeML.py
+++ StrokeML.py
@@ -3,6 +3,7 @@ import pandas as pd
 print("hello")
 data = pd.read_csv("brain_stroke.csv")
 
+print('TEst')
 print(data.columns)
 #data.drop(['work_type'], axis=1) cmd isn't working for some reason
 #print(data.columns)
