bd3ccc29f9fe9114212cbde1721e4a0b9fb62ed6
durzal1 <67489054+durzal1@users.noreply.github.com>
2023-02-18 22:54:40 -0500

    last minute fix. Glucose and bmi levels were scaled when they weren't supposed to be

--- StrokeGraphs.py
+++ StrokeGraphs.py
@@ -44,12 +44,12 @@ scaler = MinMaxScaler()
 scaler.fit(X_train)
 
 X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
+X_test_scale = scaler.transform(X_test)
 
 
 # Predicts the stroke chances for the data
 
-y_log = model.predict(X_test)
+y_log = model.predict(X_test_scale)
 
 
 # BMI, Age, Glucose Data Set Setup
